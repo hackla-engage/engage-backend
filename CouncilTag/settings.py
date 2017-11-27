d4b83f846435114a0add9398410475811247f346
ShariqT <shariq.torres@gmail.com>
2017-11-26 19:02:03 -0800

    tests

--- CouncilTag/settings.py
+++ CouncilTag/settings.py
@@ -137,5 +137,8 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['USER'] = os.environ.get('PGUSER')
-DATABASES['default']['PASSWORD'] = os.environ.get('PGPASSWORD')
+
+if os.getlogin() == "rof":
+    DATABASES['default']['USER'] = os.environ.get('PGUSER')
+    DATABASES['default']['PASSWORD'] = os.environ.get('PGPASSWORD')
+    DATABASES['default']['host'] = 'localhost'
