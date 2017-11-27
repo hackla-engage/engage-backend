472479856b0d2a5a954ad72bceb1c81f8d166c17
ShariqT <shariq.torres@gmail.com>
2017-11-26 21:09:06 -0800

    eqwewqeq

--- CouncilTag/setting_overrides.py
+++ CouncilTag/setting_overrides.py
@@ -2,14 +2,15 @@ import os
 
 def configure_overrides(settings):
     #this is the codeship environment
-    settings['DATABASES'].update(dict(
-        {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': 'test',
-                'USER': os.environ.get('PGUSER'),
-                'PASSWORD':os.environ.get('PGPASSWORD'),
-                'HOST':'127.0.0.1',
+    if os.getlogin() == "rof":
+        settings['DATABASES'].update(dict(
+            {
+                'default': {
+                    'ENGINE': 'django.db.backends.postgresql',
+                    'NAME': 'test',
+                    'USER': os.environ.get('PGUSER'),
+                    'PASSWORD':os.environ.get('PGPASSWORD'),
+                    'HOST':'127.0.0.1',
+                }
             }
-        }
-    ))
+        ))
