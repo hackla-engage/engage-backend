be31db4f9efcf4d2e90a2b7319654d8cb318dbb3
Jeff Yamamoto <jeffmagician@gmail.com>
2018-03-11 19:42:07 -0700

    Remove frontend settings and dependencies

--- CouncilTag/frontend/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
-# Create your views here.
-def home_page(request):
-    
-    return render(request, "index.html")
