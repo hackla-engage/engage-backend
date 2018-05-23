9e83ab4fb10a98071685d5d7e7bbefa89a78723c
Eli Selkin <ejselkin@cpp.edu>
2018-05-23 14:03:55 -0700

    expose url

--- CouncilTag/settings.py
+++ CouncilTag/settings.py
@@ -95,6 +95,7 @@ else:
     DATABASES = {}
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 print(DATABASES)
+print(os.environ.get("DATABASE_URL"))
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
