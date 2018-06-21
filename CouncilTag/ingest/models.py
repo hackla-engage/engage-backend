22b143c31b53f986c05cdea95176902c509a2c67
Eli Selkin <ejselkin@cpp.edu>
2018-06-21 13:09:40 -0700

    integer pro field... 0 = con, 1 = pro, 2 = unsure or no opinion, 3...?

--- CouncilTag/ingest/models.py
+++ CouncilTag/ingest/models.py
@@ -81,4 +81,4 @@ class Message(models.Model):
     ethnicity = models.TextField(blank=True, null=True)
     date = models.PositiveIntegerField(default=0)  # Unix timestamp
     sent = models.PositiveIntegerField(default=0)  # Unix timestamp
-    pro = models.BooleanField(default=False)
+    pro = models.PositiveIntegerField(default=0, null=False)
