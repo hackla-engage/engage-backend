06232cf9f329b91fea63d1429c2e1011e9b4153c
Teddy Crepineau <teddy.crepineau@gmail.com>
2018-08-12 21:31:23 -0700

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   writePdf.py
    
     Changes not staged for commit:
    	modified:   models.py

--- CouncilTag/ingest/writePdf.py
+++ CouncilTag/ingest/writePdf.py
@@ -6,7 +6,7 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_JUSTIFY, TA_LEFT
 from reportlab.pdfbase import pdfmetrics
-from CouncilTag.ingest.models import Message, Committee
+from CouncilTag.ingest.models import Message, Committee 
 from CouncilTag.api.utils import send_mail
 import io
 from datetime import datetime, date
@@ -17,17 +17,22 @@ import os
 def paragraphize_comments(comments, contents):
     ps_email = ParagraphStyle(
         "Email", fontSize=10, leftIndent=0.25 * inch, textColor="#000000")
+    ps_name = ParagraphStyle(
+        "name", fontSize=10, leftIndent=0.25 * inch, spaceBefore=10, textColor="#000000")
     ps_comment = ParagraphStyle(
         "Comment", fontSize=10, leftIndent=0.25 * inch, textColor="#000000")
     for comment in comments:
         if comment.email is not None:
-            email = "Email: " + comment.email
+            name = "Name: " + comment.first_name + " " + comment.last_name
+            email = "Email: <a href='mailto:" + str(comment.user.email) + "'>" + comment.user.email + "</a>"
             comment = "Comment: " + comment.content
         elif comment.user is not None:
-            email = "Email: " + comment.user.email
+            name = "Name: " + comment.first_name + " " + comment.last_name
+            email = "Email: <a href='mailto:" + str(comment.user.email) + "'>" + comment.user.email + "</a>"
             comment = "Comment: " + comment.content
         else:
             continue
+        contents.append(Paragraph(name, ps_name))
         contents.append(Paragraph(email, ps_email))
         contents.append(Paragraph(comment, ps_comment))
     return contents
@@ -54,11 +59,19 @@ def writePdfForAgendaItems(agenda_items):
             "title", fontSize=14, alignment=TA_JUSTIFY, spaceAfter=0.2 * inch)
         ps_id = ParagraphStyle("id", fontSize=10)
         ps_pro = ParagraphStyle(
-            'pro', fontSize=10, backColor='#0000FF', textColor="#FFFFFF")
+            'pro', fontSize=10, backColor='#27ae60', textColor="#FFFFFF", spaceBefore=7,borderPadding= (5,5,5,5))
         ps_con = ParagraphStyle(
-            'con', fontSize=10, backColor='#FF0000', textColor='#FFFFFF')
+            'con', fontSize=10, backColor='#c0392b', textColor='#FFFFFF',spaceBefore=7,borderPadding= (5,5,5,5))
         ps_need_info = ParagraphStyle(
-            'need_info', fontSize=10, backColor='#000000', textColor='#FFFFFF')
+            'need_info', fontSize=10, backColor='#000000', textColor='#FFFFFF',spaceBefore=7,borderPadding= (5,5,5,5))
+        
+        cover_page_title = ParagraphStyle("cover", fontSize=21, alignment=TA_CENTER, textColor="#000000")
+        cover_page_subtitle = ParagraphStyle("cover", fontSize=16, spaceBefore= 15, alignment=TA_CENTER, textColor="#000000")
+
+        contents.append(Spacer(0, 3 * inch))
+        contents.append(Paragraph("Agenda Items - Comment Submissions",cover_page_title))
+        contents.append(Paragraph(f"{datetime.fromtimestamp(agenda_items[0].agenda.meeting_time).strftime('%m/%d/%Y')} Council Meeting - Submissions for {today.strftime('%m/%d/%Y')}",cover_page_subtitle))
+        contents.append(PageBreak())
 
         for upcoming_agenda_item in agenda_items:
             contents.append(
