cbe62eb58d48eb2863c461ae0fa64b135534603b
Eli Selkin <ejselkin@cpp.edu>
2018-08-31 22:45:21 -0700

    testing

--- CouncilTag/settings.py
+++ CouncilTag/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,8 +121,7 @@ DATABASES = {
         },
     },
 }
-
-print(os.environ.get('DATABASE_URL'))
+print(DATABASES['default']['TEST'])
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
