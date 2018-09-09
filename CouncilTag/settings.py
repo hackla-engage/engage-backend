7ee3ede83fffbb0684908615467e2b99a6d9f3e8
Eli Selkin <ejselkin@cpp.edu>
2018-09-09 14:03:17 -0700

    add TEST global

--- CouncilTag/settings.py
+++ CouncilTag/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-TEST = FALSE
+TEST = False
 if os.environ.get("CouncilTag") == 'debug':
     DEBUG = True
 if os.environ.get("CouncilTag") == 'test':
