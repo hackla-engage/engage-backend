d8dc652b7bbbfbdc03fee87906d2e49ef76cef22
Kaelan <kaelan.cotter@gmail.com>
2019-01-23 21:35:13 -0800

    Remove lines instead of comment out #147

--- CouncilTag/wsgi.py
+++ CouncilTag/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CouncilTag.settings")
 
 application = get_wsgi_application()
-# application = WhiteNoise(application, root='./CouncilTag/static')
